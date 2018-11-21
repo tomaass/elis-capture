c01979556b2c738d6faa1880b2b15db9fc8a5db2
tomaass <tomas@sorejs.cz>
2018-11-21 18:35:02 +0100

    Add template for login screen

--- App.js
+++ App.js
@@ -8,15 +8,6 @@ import React from 'react';
 const App = () => (
   <NativeRouter>
     <View style={styles.container}>
-      <View style={styles.nav}>
-        <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
-          <Text>Login</Text>
-        </Link>
-        <Link to="/camera" underlayColor="#f0f4f7" style={styles.navItem}>
-          <Text>About</Text>
-        </Link>
-      </View>
-
       <Route exact path="/" component={Login} />
       <Route path="/camera" component={Camera} />
     </View>
@@ -25,27 +16,8 @@ const App = () => (
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 25,
-    padding: 10
-  },
-  header: {
-    fontSize: 20
-  },
-  nav: {
-    flexDirection: "row",
-    justifyContent: "space-around"
-  },
-  navItem: {
     flex: 1,
-    alignItems: "center",
-    padding: 10
-  },
-  subNavItem: {
-    padding: 5
-  },
-  topic: {
-    textAlign: "center",
-    fontSize: 15
+    flexDirection: 'column',
   }
 });
 
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,85 @@
 /* @flow */
-import { Text } from 'react-native';
+import { Text, TextInput, StyleSheet, View, Button } from 'react-native';
 import React from 'react';
+import Logo from './rossumLogo';
 
-const Login = () => <Text>Login</Text>;
+const Login = () => (
+  <View style={styles.container}>
+    <View style={styles.logoWrapper}>
+      <View style={styles.logoText}>
+        <Text style={[styles.whiteText, styles.boldText]}>
+          ELIS
+        </Text>
+        <Text style={styles.whiteText}>
+          CAPTURE
+        </Text>
+      </View>
+      <View style={styles.logoText}>
+        <Text style={{ color: 'white' }} >
+          by ROSSUM
+        </Text>
+      </View>
+    </View>
+    <View style={styles.formWrapper}>
+      <View style={{ flex: 1 }}>
+        <TextInput
+          style={{ color: '#979797' }}
+          placeholder='E-mail'
+          placeholderTextColor= '#979797'
+        />
+      </View>
+      <View style={{ flex: 1 }}>
+        <TextInput
+          style={{ color: '#979797' }}
+          placeholder='Password'
+          placeholderTextColor= '#979797'
+        />
+      </View>
+      <View style={{ flex: 1 }}>
+        <Button
+          title='Login'
+          onPress={() => {}}
+          color='white'
+          style={{ backgroundColor: '#2f72ff' }}
+        />
+      </View>
+    </View>
+  </View>
+  )
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#1b1922',
+    marginTop: 20,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    flex: 1,
+  },
+
+  logoWrapper: {
+    flex: 1,
+    alignItems: 'center'
+  },
+
+  formWrapper: {
+    flex: 1,
+    alignItems: 'center'
+  },
+
+  logoText: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+
+  whiteText: {
+    color: 'white',
+    fontSize: 20
+  },
+
+  boldText: {
+    fontWeight: 'bold',
+    fontSize: 20
+  }
+})
 
 export default Login;
