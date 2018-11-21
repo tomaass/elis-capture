3922962e051613abc12363f30152be840b6c5dec
tomaass <tomas@sorejs.cz>
2018-11-21 14:13:45 +0100

    Add routing

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import React from 'react';
+import { NativeRouter, Route, Link } from "react-router-native";
 import { StyleSheet, Text, View } from 'react-native';
+import Camera from './components/Camera';
+import Login from './components/Login';
+import React from 'react';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Elis Native</Text>
+const App = () => (
+  <NativeRouter>
+    <View style={styles.container}>
+      <View style={styles.nav}>
+        <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
+          <Text>Login</Text>
+        </Link>
+        <Link to="/camera" underlayColor="#f0f4f7" style={styles.navItem}>
+          <Text>About</Text>
+        </Link>
       </View>
-    );
-  }
-}
+
+      <Route exact path="/" component={Login} />
+      <Route path="/camera" component={Camera} />
+    </View>
+  </NativeRouter>
+);
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 25,
+    padding: 10
   },
+  header: {
+    fontSize: 20
+  },
+  nav: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  navItem: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10
+  },
+  subNavItem: {
+    padding: 5
+  },
+  topic: {
+    textAlign: "center",
+    fontSize: 15
+  }
 });
+
+export default App;
---
+++ components/Camera/index.js
@@ -0,0 +1,6 @@
+import { Text } from 'react-native';
+import React from 'react';
+
+const Camera = () => <Text>Camera</Text>;
+
+export default Camera
