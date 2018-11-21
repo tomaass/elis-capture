ab4574d25d19d24e285a8d708d54f65b30c6cb61
zepod <albert.nemec2457@gmail.com>
2018-11-21 21:14:24 +0100

    Connect components with login actions

--- src/components/Camera/index.js
+++ src/components/Camera/index.js
@@ -2,12 +2,6 @@
 import { Text, View, StyleSheet } from 'react-native';
 import React from 'react';
 
-const Camera = () => (
-  <View style={styles.container}>
-    <Text style={{ color: 'white', fontSize: 20 }}>There will be a camera</Text>;
-  </View>
-)
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#1b1922',
@@ -15,6 +9,14 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     flex: 1,
   },
-})
+});
+
+const Camera = () => (
+  <View style={styles.container}>
+    <Text style={{ color: 'white', fontSize: 20 }}>
+      There will be a camera
+    </Text>
+  </View>
+);
 
 export default Camera;
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,85 +1,115 @@
 /* @flow */
-import { Text, TextInput, StyleSheet, View, Button } from 'react-native';
-import { withRouter } from 'react-router-native';
-import Logo from './rossumLogo';
 import React from 'react';
-
-const Login = withRouter(({ history }) => (
-  <View style={styles.container}>
-    <View style={styles.logoWrapper}>
-      <View style={styles.logoText}>
-        <Text style={[styles.whiteText, styles.boldText]}>
-          ELIS
-        </Text>
-        <Text style={styles.whiteText}>
-          CAPTURE
-        </Text>
-      </View>
-      <View style={styles.logoText}>
-        <Text style={{ color: 'white' }} >
-          by ROSSUM
-        </Text>
-      </View>
-    </View>
-    <View style={styles.formWrapper}>
-      <View style={{ flex: 1 }}>
-        <TextInput
-          style={{ color: '#979797' }}
-          placeholder='E-mail'
-          placeholderTextColor= '#979797'
-        />
-      </View>
-      <View style={{ flex: 1 }}>
-        <TextInput
-          style={{ color: '#979797' }}
-          placeholder='Password'
-          placeholderTextColor= '#979797'
-        />
-      </View>
-      <View style={{ flex: 1 }}>
-        <Button
-          title='Login'
-          onPress={() => { history.push('/camera') }}
-          color='white'
-          style={{ backgroundColor: '#2f72ff' }}
-        />
-      </View>
-    </View>
-  </View>
-  ))
+import {
+  Text,
+  TextInput,
+  StyleSheet,
+  View,
+  Button,
+} from 'react-native';
+import { connect } from 'react-redux';
+import { loginUser } from '../../redux/modules/user/actions';
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#1b1922',
     flexDirection: 'column',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     flex: 1,
   },
 
   logoWrapper: {
-    flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
   },
 
   formWrapper: {
+    alignItems: 'center',
     flex: 1,
-    alignItems: 'center'
   },
 
   logoText: {
-    flex: 1,
     flexDirection: 'row',
   },
 
   whiteText: {
     color: 'white',
-    fontSize: 20
+    fontSize: 20,
   },
 
   boldText: {
     fontWeight: 'bold',
-    fontSize: 20
-  }
-})
+    fontSize: 20,
+  },
+});
 
-export default Login;
+type State = { username: string, password: string }
+type Props = { login: Function }
+
+class Login extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  render() {
+    const { username, password } = this.state;
+    const { login } = this.props;
+    return (
+      <View style={styles.container}>
+        <View style={styles.logoWrapper}>
+          <View style={styles.logoText}>
+            <Text style={[styles.whiteText, styles.boldText]}>
+              ELIS
+            </Text>
+            <Text style={styles.whiteText}>
+              CAPTURE
+            </Text>
+          </View>
+          <View style={styles.logoText}>
+            <Text style={{ color: 'white' }}>
+              by ROSSUM
+            </Text>
+          </View>
+        </View>
+        <View style={styles.formWrapper}>
+          <View style={{ flex: 1 }}>
+            <TextInput
+              value={username}
+              style={{ color: '#979797' }}
+              placeholder="E-mail"
+              onChangeText={value => this.setState({ username: value })}
+              placeholderTextColor="#979797"
+            />
+          </View>
+          <View style={{ flex: 1 }}>
+            <TextInput
+              value={password}
+              style={{ color: '#979797' }}
+              placeholder="Password"
+              onChangeText={value => this.setState({ password: value })}
+              placeholderTextColor="#979797"
+              secureTextEntry
+            />
+          </View>
+          <View style={{ flex: 1 }}>
+            <Button
+              title="Login"
+              onPress={() => login({ password, username })}
+              color="white"
+              style={{ backgroundColor: '#2f72ff' }}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  login: (...args) => dispatch(loginUser(...args)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
