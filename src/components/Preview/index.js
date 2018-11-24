47f90968ec6f33475e76c657c2148366b4499ecd
zepod <albert.nemec2457@gmail.com>
2018-11-25 00:05:01 +0100

    Use carousel for preview

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -1,7 +1,8 @@
 /* @flow */
 import React from 'react';
-import { View, ImageBackground } from 'react-native';
+import { View, Image, Dimensions } from 'react-native';
 import { Button } from 'react-native-elements';
+import Carousel from 'react-native-snap-carousel';
 
 type Props = {
   send: Function,
@@ -9,45 +10,77 @@ type Props = {
   photoUri: string,
 }
 
-const Preview = ({ send, remove, photoUri }: Props) => (
-  <View style={{ flex: 1, backgroundColor: '#1b1922' }}>
-    <View style={{
-      margin: '10%',
-      marginTop: '25%',
-      flex: 9,
-      marginBottom: 0,
-    }}
-    >
-      <ImageBackground
-        style={{ flex: 1 }}
-        source={{ uri: photoUri }}
-      />
-    </View>
-    <View style={{
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      flex: 1,
-    }}
-    >
-      <Button
-        backgroundColor="transparent"
-        onPress={remove}
-        icon={{ name: 'delete', size: 20 }}
-        title="Delete"
-        fontSize={18}
-        buttonStyle={{ height: 40 }}
-      />
-      <Button
-        onPress={send}
-        borderRadius={10}
-        backgroundColor="#2f72ff"
-        icon={{ name: 'send', size: 20 }}
-        title="Send"
-        fontSize={18}
-        buttonStyle={{ height: 40 }}
-      />
-    </View>
-  </View>
-);
+type State = {
+  currentIndex: number,
+}
 
+class Preview extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { currentIndex: 0 };
+  }
+
+  render() {
+    const { send, remove, photoUri } = this.props;
+    const { width, height } = Dimensions.get('window');
+    const { currentIndex } = this.state;
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#1b1922',
+          height: '100%',
+        }}
+      >
+        <Carousel
+          layout="default"
+          data={[{ uri: photoUri }, { uri: photoUri }]}
+          onSnapToItem={index =>
+            this.setState(() => ({ currentIndex: index }))}
+          swipeThreshold={1}
+          vertical
+          sliderHeight={height}
+          itemWidth={width}
+          itemHeight={height * 0.6}
+          containerCustomStyle={{ flex: 9, marginHorizontal: 30 }}
+          sliderWidth={width - 60}
+          renderItem={({ item, index }) => (
+            <Image
+              style={{
+                flex: 1,
+                resizeMode: 'contain',
+                opacity: index === currentIndex ? 1 : 0.5,
+              }}
+              source={{ uri: item.uri }}
+            />
+          )}
+        />
+        {/* <View style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          flex: 1,
+        }}
+        >
+          <Button
+            backgroundColor="transparent"
+            onPress={remove}
+            icon={{ name: 'delete', size: 20 }}
+            title="Delete"
+            fontSize={18}
+            buttonStyle={{ height: 40 }}
+          />
+          <Button
+            onPress={send}
+            borderRadius={10}
+            backgroundColor="#2f72ff"
+            icon={{ name: 'send', size: 20 }}
+            title="Send"
+            fontSize={18}
+            buttonStyle={{ height: 40 }}
+          />
+        </View> */}
+      </View>
+    );
+  }
+}
 export default Preview;
